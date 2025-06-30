a78ae954ca30c4dd85c6902ffb036d923ed1dd20
geekp8 <82133404+geekp8@users.noreply.github.com>
2025-06-30 17:14:36 +0530

    Update and rename Machine Learning to DataSet1.py

---
+++ DataSet1.py
@@ -0,0 +1,15 @@
+
+from sklearn.datasets import load_iris
+
+def main():
+    dataset=load_iris()
+
+    print("Independent(Features) variable names are:")
+    print(dataset.feature_names)
+
+    print("Dependent (Labels) variable names are:")
+    print(dataset.target_names)
+
+
+if __name__=="__main__":
+    main()
